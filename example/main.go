e031b27b7b599f00e2f46ef4f59a5d646d2652eb
Kārlis Gaigals <karlis@nic.lv>
2025-02-17 13:11:29 +0200

    disable stdout & stderr

--- example/main.go
+++ example/main.go
@@ -10,8 +10,17 @@ import (
 // Set as false if this is docker build. Syslogs are not supported on docker.
 const isLocalBuild = false
 
+// Disable stdout and stderr output. Does not impact syslogger.
+const disableStdOutput = false
+
 func main() {
-	err := logger.NewGlobalLogger("test", "global.log", isLocalBuild, syslog.LOG_INFO)
+	err := logger.NewGlobalLogger(
+		"test",
+		"global.log",
+		isLocalBuild,
+		disableStdOutput,
+		syslog.LOG_USER,
+	)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -20,13 +29,20 @@ func main() {
 		"test",
 		"instance.log",
 		isLocalBuild,
+		disableStdOutput,
 		syslog.LOG_INFO|syslog.LOG_USER,
 	)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	l2, err := logger.NewLogger("test", "", isLocalBuild, syslog.LOG_INFO|syslog.LOG_USER)
+	l2, err := logger.NewLogger(
+		"test",
+		"",
+		isLocalBuild,
+		disableStdOutput,
+		syslog.LOG_INFO|syslog.LOG_USER,
+	)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -44,7 +60,13 @@ func main() {
 	l2.Println("instance2", "println")
 	l2.Printf("instance2 printlf=%d\n", 21)
 
-	l3 := logger.NewLoggerOrFatal("test", "test.log", isLocalBuild, syslog.LOG_INFO|syslog.LOG_INFO)
+	l3 := logger.NewLoggerOrFatal(
+		"test",
+		"test.log",
+		isLocalBuild,
+		disableStdOutput,
+		syslog.LOG_INFO|syslog.LOG_INFO,
+	)
 
 	l3.Debug("instance3 debug test")
 }
